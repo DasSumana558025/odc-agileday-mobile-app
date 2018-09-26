40eabe0a7aab4b2eed8e7160dabcc8d5b7d578e7
DasSumana558025 <sumana.das0201@gmail.com>
2018-09-26 07:43:15 +0530

    agenda page changes

--- src/pages/agenda/agenda.ts
+++ src/pages/agenda/agenda.ts
@@ -18,13 +18,45 @@ import 'rxjs/add/operator/map';
 })
 export class AgendaPage implements OnInit {
   topics: any[];
+  timeSlotSpecificTopics:any[] = [];
+  filterValues = ['11:00 To 11:15','11:15 To 12:15','12:15 To 13:15','13.15 to 14.15','14:15 To 15:15','15:15 To 16:15','16:15 To 16:45'];
   constructor(public navCtrl: NavController, public navParams: NavParams,public apiProvider: ServicesProvider,public events: Events) {
   }
 
   ngOnInit(){
     this.apiProvider.getAllTopics().map(res=>res.json()).subscribe(data => {
       this.topics = data;
-      console.log(this.topics);
+      for(var i = 0; i < this.topics.length; i++){
+        for(var j = 0; j < this.topics[i].presenters.length; j++)
+        {
+          if(this.topics[i].presenterName === undefined){
+            this.topics[i]["presenterName"]= this.topics[i].presenters[j].firstName + " " + this.topics[i].presenters[j].lastName;
+          }
+          else
+          {
+            this.topics[i].presenterName = this.topics[i].presenterName + " , " + this.topics[i].presenters[j].firstName + " " + this.topics[i].presenters[j].lastName;
+          }
+        }
+      }
+      for(var i=0;i<this.filterValues.length;i++){
+        let sessions = this.topics.filter(x=> x.timeSlot == this.filterValues[i]);
+        console.log(sessions);
+        if(sessions != undefined && sessions.length == 0){
+          this.timeSlotSpecificTopics.push({"timeSlot":this.filterValues[i],"room1": 'Lunch Break'});
+        }
+        else if(sessions.length == 1)
+        {
+          this.timeSlotSpecificTopics.push({"timeSlot":this.filterValues[i],"room1": sessions[0].description,
+                                        "room1Presenter":sessions[0].presenterName});
+        }
+        else if(sessions.length >1){
+          this.timeSlotSpecificTopics.push({"timeSlot":this.filterValues[i],"room1": sessions[0].description,
+                                        "room1Presenter":sessions[0].presenterName,"room2":sessions[1].description,
+                                        "room2Presenter":sessions[1].presenterName});
+        }
+        
+      }
+      console.log(this.timeSlotSpecificTopics);
      
   });
   }
