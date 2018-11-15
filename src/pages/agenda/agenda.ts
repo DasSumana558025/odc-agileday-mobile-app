4acd3a98901caf7c179fea7c8c2405ffc5c0f861
trupti <trupti.dipke@atos.net>
2018-11-16 01:35:59 +0530

    Make Agenda Page Dyanamic on Session Room

--- src/pages/agenda/agenda.ts
+++ src/pages/agenda/agenda.ts
@@ -19,43 +19,41 @@ import 'rxjs/add/operator/map';
 export class AgendaPage implements OnInit {
   topics: any[];
   timeSlotSpecificTopics:any[] = [];
+  roomSpecificTopics:any[] = [];
+
   filterValues = ['11:00 To 11:15','11:15 To 12:15','12:15 To 13:15','13.15 to 14.15','14:15 To 15:15','15:15 To 16:15','16:15 To 16:45'];
+  roomNos = ['TR02','TR03'];
+  totalRoomNo : string ;
   constructor(public navCtrl: NavController, public navParams: NavParams,public apiProvider: ServicesProvider,public events: Events) {
   }
 
   ngOnInit(){
     this.apiProvider.getAllTopics().map(res=>res.json()).subscribe(data => {
       this.topics = data;
-      for(var i = 0; i < this.topics.length; i++){
-        for(var j = 0; j < this.topics[i].presenters.length; j++)
-        {
-          if(this.topics[i].presenterName === undefined){
-            this.topics[i]["presenterName"]= this.topics[i].presenters[j].firstName + " " + this.topics[i].presenters[j].lastName;
-          }
-          else
-          {
-            this.topics[i].presenterName = this.topics[i].presenterName + " , " + this.topics[i].presenters[j].firstName + " " + this.topics[i].presenters[j].lastName;
-          }
-        }
-      }
+      this.totalRoomNo = this.roomNos.length.toString();
       for(var i=0;i<this.filterValues.length;i++){
         let sessions = this.topics.filter(x=> x.timeSlot == this.filterValues[i]);
-        if(sessions != undefined && sessions.length == 0){
-          this.timeSlotSpecificTopics.push({"timeSlot":this.filterValues[i],"room1": 'Lunch Break'});
+        let roomSpecificTopic = [];
+        for(var j=0;j<this.roomNos.length;j++){
+          let roomspecificsessions = sessions.find(x=> x.roomNumber == this.roomNos[j]);
+		      if(roomspecificsessions !== undefined){
+            for(var k = 0; k < roomspecificsessions.presenters.length; k++)
+            {
+              if(roomspecificsessions.presenterName === undefined){
+                roomspecificsessions["presenterName"]= roomspecificsessions.presenters[k].firstName + " " + roomspecificsessions.presenters[k].lastName;
+              }
+              else
+              {
+               roomspecificsessions.presenterName = roomspecificsessions.presenterName + " , " + roomspecificsessions.presenters[k].firstName + " " + roomspecificsessions.presenters[k].lastName;
+              } 
+           }
+		        roomSpecificTopic.push({"room":this.roomNos[j],"topic":roomspecificsessions.description,"presentername":roomspecificsessions.presenterName});
+		      }
         }
-        else if(sessions.length == 1)
-        {
-          this.timeSlotSpecificTopics.push({"timeSlot":this.filterValues[i],"room1": sessions[0].description,
-                                        "room1Presenter":sessions[0].presenterName});
-        }
-        else if(sessions.length >1){
-          this.timeSlotSpecificTopics.push({"timeSlot":this.filterValues[i],"room1": sessions[0].description,
-                                        "room1Presenter":sessions[0].presenterName,"room2":sessions[1].description,
-                                        "room2Presenter":sessions[1].presenterName});
-        }
-        
+        this.timeSlotSpecificTopics.push({"timeSlot":this.filterValues[i],"roomDetails":roomSpecificTopic});
       }
       console.log(this.timeSlotSpecificTopics);
+
      
   });
   }
